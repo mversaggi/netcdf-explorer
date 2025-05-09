6b532d31328e39115753dddeec8b305aefc8771d
mversaggi <backster18@aim.com>
2025-05-09 00:27:21 -0400

    Fix .nc file uploading, add netCDF4 dependency

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, render_template
+import tempfile
 import xarray as xr
 import os
 
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -11,7 +11,10 @@ def index():
     if request.method == "POST":
         file = request.files["file"]
         if file:
-            ds = xr.open_dataset(file)
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".nc") as tmp:
+                file.save(tmp.name)
+                tmp_path = tmp.name
+            ds = xr.open_dataset(tmp_path, engine="netcdf4")
             summary = str(ds)
     return render_template("index.html", summary=summary)
 
